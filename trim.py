a2461a3bd0a83dcf863c9a7c4ff968d3ec69fe6e
calvinyhchen <calvin.yh.chen@gmail.com>
2018-02-21 14:57:00 -0800

    trim var

--- trim.py
+++ trim.py
@@ -1,18 +1,24 @@
 import csv
+import numpy as np
 
 dic = {}
-dic_rate = {}
+dic_rate_index = {}
+all_rate = []
+index = 0
 with open("ml-latest-small/ratings_new.csv", "r") as csvfile:
   content = csv.reader(csvfile)
   for row in content:
     user, movie, rate = row[0], row[1], row[2]
     if movie not in dic:
       dic[movie] = 1
-      dic_rate[movie] = rate
+      dic_rate_index[movie] = index
+      index += 1
+      all_rate.append([float(rate)])
     else:
       dic[movie] += 1 
-      dic_rate[movie] = 10
+      all_rate[dic_rate_index[movie]].append(float(rate))
 csvfile.close()
+print(all_rate)
 
 with open("ml-latest-small/ratings_new.csv", "r") as csvfile:
   content = csv.reader(csvfile)
@@ -51,8 +57,11 @@ with open("ml-latest-small/ratings_new.csv", "r") as csvfile:
     writer = csv.writer(file)
     for row in content:
       userId, movId, rating, name = row[0], row[1], row[2], row[3]
-      if dic[movId] > 4 and dic_rate[movId] == 10:
-        writer.writerow([userId, movId, rating, name])
+      if dic[movId] > 4:
+        variance = np.var(all_rate[dic_rate_index[movId]])
+        print(variance)
+        if variance > 2:
+          writer.writerow([userId, movId, rating, name])
         
   file.close()
 
